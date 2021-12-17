e6d0b2b204534ee63fefc36f6408f3b22dc60d35
Vyas Raina <sayvaniar@gmail.com>
2021-12-17 15:10:59 +0000

    errant

--- gec_tools.py
+++ gec_tools.py
@@ -1,11 +1,15 @@
+import random
+import errant
+
 def get_sentences(data_path, num=None):
     with open(data_path, 'r') as f:
         lines = f.readlines()
+    if num is not None:
+        random.seed(10)
+        lines = random.shuffle(lines)
+        lines = lines[:num]
     texts = [' '.join(l.rstrip('\n').split()[1:]) for l in lines]
     ids = [l.rstrip('\n').split()[0] for l in lines]
-    if num is not None:
-        texts = texts[:num]
-        ids = ids[:num]
     return ids, texts
 
 def correct(model, sentence, gen_args):
@@ -18,3 +22,7 @@ def count_edits(input, prediction):
     '''
     Count number of edits
     '''
+    annotator = errant.load('en')
+    alignment = annotator.align(input, prediction)
+    edits = annotator.merge(alignment)
+    return len(edits)
