4ae873421c11d4238ae028bd77b3de8139ce9ca0
Vyas Raina <sayvaniar@gmail.com>
2022-01-18 14:26:37 +0000

    by part stats

--- eval_uni_attack.py
+++ eval_uni_attack.py
@@ -3,6 +3,8 @@ Evaluate universal adversarial attack.
 
 Evaluate by counting average number of edits between original input
 (with attack phrase) and  GEC model prediction
+
+Also report how many edits average in original part of sentence and adversarial part.
 '''
 
 import sys
@@ -12,15 +14,37 @@ from happytransformer import HappyTextToText, TTSettings
 import torch
 from gec_tools import get_sentences, correct, return_edits
 from statistics import mean, stdev
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-def print_stats(edits, name):
+def print_stats(edits, name, original_part_count, adv_part_count):
     print()
     print(f'{name}: {len(edits)} samples')
     edits_mean = mean(edits)
     edits_std = stdev(edits)
-    print(f'\nMean: {edits_mean}\t Std: {edits_std}')
+    print(f'\nTotal Edits:\tMean: {edits_mean}\t Std: {edits_std}')
+    orig_mean = mean(original_part_count)
+    orig_std = stdev(original_part_count)
+    print(f'\nOriginal Part Edits:\tMean: {orig_mean}\t Std: {orig_std}')
+    adv_mean = mean(adv_part_count)
+    adv_std = stdev(adv_part_count)
+    print(f'\nAdv Part Edits:\tMean: {adv_mean}\t Std: {adv_std}')
     print()
+
+def get_edits_by_part(original_sentence, attack_edits):
+    '''
+    Determine how many attack edits in which part of attacked sentence
+    '''
+
+    edit_strs = [e.o_str for e in attack_edits]
+    orig = 0
+    adv = 0
+    for e_str in edit_strs:
+        if original_sentence.find(e_str) == -1:
+            adv+=1
+        else:
+            orig+=1
+    return orig, adv
+
 
 if __name__ == "__main__":
 
@@ -48,6 +72,8 @@ if __name__ == "__main__":
     # Correction (prediction) for each input sentence and associated number of edits
     edit_counts = []
     edit_counts_with_attack = []
+    original_part_count = [] # for adv phrase, count of edits in non adv-part
+    adv_part_count = [] # for adv phrase, count of edits in adv-part
     for i, sent in enumerate(sentences):
 
         print(f'On {i}/{len(sentences)}')
@@ -63,9 +89,16 @@ if __name__ == "__main__":
         edits = return_edits(sent, correction)
         edits = [e.o_str+' -> '+e.c_str for e in edits]
         edit_counts.append(len(edits))
+
         edits_with_attack = return_edits(sent_with_attack, correction_with_attack)
+        original_part, adv_part = get_edits_by_part(sent, edits_with_attack)
+        original_part_count.append(original_part)
+        adv_part_count.append(adv_part)
+
         edits_with_attack = [e.o_str+' -> '+e.c_str for e in edits_with_attack]
         edit_counts_with_attack.append(len(edits_with_attack))
+
+
         print(f'Edits without attack: {edits}')
         print(f'Edits with attack: {edits_with_attack}\n')
 
@@ -81,13 +114,14 @@ if __name__ == "__main__":
         name = f'Filtered >{thresh}'
         print_stats(edits_imperfect, name)
     
-    # # Plot histogram of edit count distribution before and after attack
-    # plt.hist(edit_counts, bins=10, alpha=0.5, label='No Attack')
-    # plt.hist(edit_counts_with_attack, bins=10, alpha=0.5, label='With Attack')
-    # plt.xlabel("Edits")
-    # plt.ylabel("Count")
-    # plt.legend()
-    # plt.savefig(args.FIG, bbox_inches='tight')
+    # Plot histogram of edit count distribution before and after attack
+    plt.hist(edit_counts, bins=10, alpha=0.5, label='No Attack')
+    plt.hist(edit_counts_with_attack, bins=10, alpha=0.5, label='With Attack')
+    plt.xlabel("Edits")
+    plt.ylabel("Count")
+    plt.legend()
+    plt.savefig(args.FIG, bbox_inches='tight')
+    plt.clf()
 
 
 
