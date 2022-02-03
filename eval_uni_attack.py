2ace1ebfde57abfccc27c11ca2952e0c08ca66f7
Vyas Raina <sayvaniar@gmail.com>
2022-02-03 23:03:07 +0000

    error type

--- eval_uni_attack.py
+++ eval_uni_attack.py
@@ -4,9 +4,13 @@ Evaluate universal adversarial attack.
 Evaluate by counting average number of edits between original input
 (with attack phrase) and  GEC model prediction
 
-Also report how many edits average in original part of sentence and adversarial part.
+Also:
+- Report how many edits average in original part of sentence and adversarial part.
+- Plot histogram of error distibution before and after attack
+- Show errant error type distribution before and after attack 
 '''
 
+from email.policy import default
 import sys
 import os
 import argparse
@@ -14,7 +18,15 @@ from happytransformer import HappyTextToText, TTSettings
 import torch
 from gec_tools import get_sentences, correct, return_edits
 from statistics import mean, stdev
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from collections import defaultdict
+
+def update_edit_types(edits, edit_type_dict):
+    '''
+    Increment each type of edit that appears
+    '''
+    for e in edits:
+        edit_type_dict[e.type] += 1
 
 def print_stats(edits, name, original_part_count, adv_part_count):
     print()
@@ -52,6 +64,7 @@ if __name__ == "__main__":
     commandLineParser = argparse.ArgumentParser()
     commandLineParser.add_argument('IN', type=str, help='Path to input data')
     commandLineParser.add_argument('FIG', type=str, help='Where to save histogram plot')
+    commandLineParser.add_argument('EDIT_TYPE', type=str, help='Path to save edit type information')
     commandLineParser.add_argument('--phrase', type=str, default='', help='Universal adversarial phrase')
     args = commandLineParser.parse_args()
 
@@ -74,6 +87,8 @@ if __name__ == "__main__":
     edit_counts_with_attack = []
     original_part_count = [] # for adv phrase, count of edits in non adv-part
     adv_part_count = [] # for adv phrase, count of edits in adv-part
+    original_edit_types = defaultdict(int)
+    adv_edit_types = defaultdict(int)
     for i, sent in enumerate(sentences):
 
         print(f'On {i}/{len(sentences)}')
@@ -87,10 +102,12 @@ if __name__ == "__main__":
         print(f'Correction: {correction_with_attack}')
 
         edits = return_edits(sent, correction)
+        update_edit_types(edits, original_edit_types)
         edits = [e.o_str+' -> '+e.c_str for e in edits]
         edit_counts.append(len(edits))
 
         edits_with_attack = return_edits(sent_with_attack, correction_with_attack)
+        update_edit_types(edits_with_attack, adv_edit_types)
         original_part, adv_part = get_edits_by_part(sent, edits_with_attack)
         original_part_count.append(original_part)
         adv_part_count.append(adv_part)
@@ -107,6 +124,14 @@ if __name__ == "__main__":
     # Print stats for all samples
     print_stats(edit_counts_with_attack, 'ALL', original_part_count, adv_part_count)
 
+    # Save edit type distribution to file
+    texts = ['Original Adv']
+    for edit_type in original_edit_types.keys().sort():
+        texts.append(f'\n{original_edit_types[edit_type]} {adv_edit_types[edit_type]}')
+    with open(args.EDIT_TYPE, 'w') as f:
+            f.writelines(texts)
+    
+
     # # Print stats for samples filtered by number of actual errors (from no attack)
     # thresholds = [0, 1, 2, 3, 4, 5]
     # for thresh in thresholds:
@@ -114,14 +139,14 @@ if __name__ == "__main__":
     #     name = f'Filtered >{thresh}'
     #     print_stats(edits_imperfect, name)
     
-    # Plot histogram of edit count distribution before and after attack
-    plt.hist(edit_counts, bins=12, alpha=0.5, label='No Attack', density=True)
-    plt.hist(edit_counts_with_attack, bins=12, alpha=0.5, label='With Attack', density=True)
-    plt.xlabel("Edits")
-    plt.ylabel("Probability Density")
-    plt.legend()
-    plt.savefig(args.FIG, bbox_inches='tight')
-    plt.clf()
+    # # Plot histogram of edit count distribution before and after attack
+    # plt.hist(edit_counts, bins=12, alpha=0.5, label='No Attack', density=True)
+    # plt.hist(edit_counts_with_attack, bins=12, alpha=0.5, label='With Attack', density=True)
+    # plt.xlabel("Edits")
+    # plt.ylabel("Probability Density")
+    # plt.legend()
+    # plt.savefig(args.FIG, bbox_inches='tight')
+    # plt.clf()
 
 
 
