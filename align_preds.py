6c831223be8baadfba355306a62c75e4f6e31e76
Vyas Raina <sayvaniar@gmail.com>
2022-02-06 12:49:51 +0000

    remove punct

--- align_preds.py
+++ align_preds.py
@@ -24,11 +24,13 @@ TEXT2
 import sys
 import os
 import argparse
+import string
 
-def get_sentences_dict(data_path):
+def get_sentences_dict(data_path, remove_punct=False):
     with open(data_path, 'r') as f:
         lines = f.readlines()
     lines = [l.rstrip('\n') for l in lines]
+    exclude = set(string.punctuation)
     id2text = {}
     for l in lines:
         parts = l.split()
@@ -37,6 +39,9 @@ def get_sentences_dict(data_path):
         # remove space before full stop
         if text[-2:] == ' .':
             text = text[:-2]+'.'
+        if remove_punct:
+            # remove punctuation
+            text = ''.join(ch for ch in text if ch not in exclude)
         id2text[id] = text
     return id2text
 
@@ -63,6 +68,7 @@ if __name__ == "__main__":
     commandLineParser.add_argument('PRED', type=str, help='Path to output predicted data')
     commandLineParser.add_argument('CORR', type=str, help='Path to correct output test data')
     commandLineParser.add_argument('BASE', type=str, help='Path base for output files')
+    commandLineParser.add_argument('--remove_punct', type=str, default='no', help='remove punct or not')
     args = commandLineParser.parse_args()
 
     # Save the command run
@@ -71,10 +77,15 @@ if __name__ == "__main__":
     with open('CMDs/align_preds.cmd', 'a') as f:
         f.write(' '.join(sys.argv)+'\n')
     
+    if args.remove_punct == 'no':
+        remove_punct = False
+    else:
+        remove_punct = True
+    
     # Get sentences and align
-    inc_id2text = get_sentences_dict(args.INC)
-    pred_id2text = get_sentences_dict(args.PRED)
-    corr_id2text = get_sentences_dict(args.CORR)
+    inc_id2text = get_sentences_dict(args.INC, remove_punct=remove_punct)
+    pred_id2text = get_sentences_dict(args.PRED, remove_punct=remove_punct)
+    corr_id2text = get_sentences_dict(args.CORR, remove_punct=remove_punct)
     inc_sens, pred_sens, corr_sens = align_data(inc_id2text, pred_id2text, corr_id2text)
 
     # Save to output files
